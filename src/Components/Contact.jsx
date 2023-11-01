36abd85954620ada752522f5edff93a3217de0c4
Михаел Йорданов <gaws@mail.bg>
2023-11-01 11:22:06 +0200

    responsiveFix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
             src='https://www.google.com/maps/embed/v1/place?q=ulitsa+"Chumerna+8",+Gabrovo,+Bulgaria&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8'
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md ">
-            <div className="lg:w-1/2 px-6">
+            <div className="hidden lg:w-1/2 px-6 lg:block">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
@@ -47,7 +47,7 @@ export default function Contact() {
                 Gabrovo, Bulgaria
               </p>
             </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0 ">
+            <div className="hidden lg:w-1/2 px-6 mt-4 lg:mt-0 lg:block">
               <h2 className="title-font font-semibold text-white tracking-widest  ">
                 EMAIL
               </h2>
